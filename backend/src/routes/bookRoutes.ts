cefee6a287358fec76b6b129fb22a8fb0640a7f3
davi moreira fuzatto <davimoreiraf@gmail.com>
2025-02-17 20:28:24 -0300

    adicionando a rota GET no arquivo bookRoutes.ts

--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -3,6 +3,21 @@ import {db} from "../services/firebaseService";
 
 const router: Router = express.Router();
 
+router.get('/books', async (req: Request, res: Response) => {
+    try {
+        const booksSnapshot = await db.collection('books').get();
+        const books = booksSnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+
+        return res.status(200).json(books);
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "Erro desconhecido";
+        return res.status(500).json({ error: errorMessage });
+    }
+});
+
 router.post('/books', async (req: Request, res: Response) => {
     try {
         const snapshot = await db.collection('books').get();
