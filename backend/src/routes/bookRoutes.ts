92e823a7a66812a6f5f455e45a01daa705bf0c12
Davimf7221 <64716204+davimf721@users.noreply.github.com>
2025-02-18 11:38:56 +0000

    adicionando logs para a verificação de possiveis erros ao chamar GET e POST

--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -3,20 +3,8 @@ import {db} from "../services/firebaseService";
 
 const router: Router = express.Router();
 
-router.get('/books', async (req: Request, res: Response) => {
-    try {
-        const booksSnapshot = await db.collection('books').get();
-        const books = booksSnapshot.docs.map(doc => ({
-            id: doc.id,
-            ...doc.data()
-        }));
-        return res.status(200).json(books);
-    } catch (error) {
-        return res.status(500).json({ error: "Erro ao buscar livros" });
-    }
-});
-
 router.post('/books', async (req: Request, res: Response) => {
+    console.log('POST /api/books chamado');
     try {
         const snapshot = await db.collection('books').get();
         const books = snapshot.docs.map(doc => ({
@@ -28,6 +16,20 @@ router.post('/books', async (req: Request, res: Response) => {
     } catch (error: unknown) {
         const errorMessage = error instanceof Error ? error.message : "Erro desconhecido";
         return res.status(500).json({ error: errorMessage });
+    }
+});
+
+router.get('/books', async (req: Request, res: Response) => {
+    console.log('GET /api/books chamado');
+    try {
+        const booksSnapshot = await db.collection('books').get();
+        const books = booksSnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        return res.status(200).json(books);
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao buscar livros" });
     }
 });
 
