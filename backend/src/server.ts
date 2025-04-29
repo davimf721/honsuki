6427575d1814cfa06fd1bfc2b155bb0cefe13a79
davi moreira fuzatto <davimoreiraf@gmail.com>
2025-04-28 21:36:53 -0400

    fixing build problem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import cors from 'cors';
+
 import mainRouter from './routes/routes';
 
 const app = express();
 
 // Middlewares essenciais
-app.use(cors());
 app.use(express.json());
 
 // Configuração das rotas
